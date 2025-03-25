39b0eddd6439d7f870430a125043cc41c48118a6
Antonio Mika <me@antoniomika.me>
2025-03-24 22:56:04 -0400

    Rewrite logic

--- go.mod
+++ go.mod
@@ -3,13 +3,13 @@ module github.com/picosh/pipemgr
 go 1.23.1
 
 require (
-	github.com/docker/docker v27.2.0+incompatible
-	github.com/picosh/utils v0.0.0-20241118014950-9515a3e4c5f9
+	github.com/antoniomika/syncmap v1.0.0
+	github.com/docker/docker v28.0.2+incompatible
+	github.com/picosh/utils v0.0.0-20241120033529-8ca070c09bf4
 )
 
 require (
 	github.com/Microsoft/go-winio v0.6.2 // indirect
-	github.com/antoniomika/syncmap v1.0.0 // indirect
 	github.com/containerd/log v0.1.0 // indirect
 	github.com/distribution/reference v0.6.0 // indirect
 	github.com/docker/go-connections v0.5.0 // indirect
@@ -22,16 +22,16 @@ require (
 	github.com/moby/term v0.5.0 // indirect
 	github.com/morikuni/aec v1.0.0 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
-	github.com/opencontainers/image-spec v1.1.0 // indirect
+	github.com/opencontainers/image-spec v1.1.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.54.0 // indirect
-	go.opentelemetry.io/otel v1.32.0 // indirect
+	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.60.0 // indirect
+	go.opentelemetry.io/otel v1.35.0 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp v1.32.0 // indirect
-	go.opentelemetry.io/otel/metric v1.32.0 // indirect
-	go.opentelemetry.io/otel/sdk v1.32.0 // indirect
-	go.opentelemetry.io/otel/trace v1.32.0 // indirect
-	golang.org/x/crypto v0.29.0 // indirect
-	golang.org/x/sys v0.27.0 // indirect
+	go.opentelemetry.io/otel/metric v1.35.0 // indirect
+	go.opentelemetry.io/otel/trace v1.35.0 // indirect
+	golang.org/x/crypto v0.36.0 // indirect
+	golang.org/x/sys v0.31.0 // indirect
 	golang.org/x/time v0.8.0 // indirect
 	gotest.tools/v3 v3.5.1 // indirect
 )
--- main.go
+++ main.go
@@ -12,14 +12,174 @@ import (
 	"os/signal"
 	"regexp"
 	"strings"
+	"sync"
 	"time"
 
+	"github.com/antoniomika/syncmap"
 	ct "github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/events"
 	"github.com/docker/docker/client"
 	"github.com/docker/docker/pkg/stdcopy"
 	"github.com/picosh/utils/pipe"
 )
+
+type LogHandler struct {
+	Ctx         context.Context
+	Cancel      context.CancelFunc
+	Logger      *slog.Logger
+	ContainerID string
+	LogClients  *LogClients
+}
+
+func (lh *LogHandler) Handle() {
+	for {
+		select {
+		case <-lh.Ctx.Done():
+			lh.Logger.Info("context cancelled, exiting handler")
+			return
+		default:
+			containerInfo, err := lh.LogClients.DockerClient.ContainerInspect(lh.Ctx, lh.ContainerID)
+			if err != nil {
+				lh.Logger.Error(
+					"unable to inspect container info",
+					slog.Any("error", err),
+				)
+				lh.LogClients.RemoveContainer(lh.ContainerID)
+				return
+			}
+
+			lh.Logger.Debug(
+				"container info",
+				slog.Any("container_info", containerInfo),
+			)
+
+			isEnabled := false
+			if enabledRaw, ok := containerInfo.Config.Labels["pipemgr.enable"]; ok {
+				if enabledRaw == "true" {
+					isEnabled = true
+				}
+			}
+
+			if !isEnabled {
+				lh.LogClients.RemoveContainer(lh.ContainerID)
+				return
+			}
+
+			filterStr := ""
+			if filterRaw, ok := containerInfo.Config.Labels["pipemgr.filter"]; ok {
+				filterStr = strings.TrimSpace(filterRaw)
+			}
+
+			filter, err := regexp.Compile(filterStr)
+			if err != nil {
+				lh.Logger.Error("invalid regex provided to pipemgr.filter", "err", err, "filter", filterStr)
+			}
+
+			lh.Logger.Info("connecting to logs", "container", lh.ContainerID)
+
+			opts := ct.LogsOptions{
+				ShowStdout: true,
+				ShowStderr: true,
+				Follow:     true,
+				Since:      "0m",
+			}
+
+			readCloser, err := lh.LogClients.DockerClient.ContainerLogs(lh.Ctx, lh.ContainerID, opts)
+			if err != nil {
+				lh.Logger.Error("unable to fetch container logs", "err", err)
+				continue
+			}
+
+			r, w := io.Pipe()
+
+			var wg sync.WaitGroup
+
+			wg.Add(1)
+			go func() {
+				defer func() {
+					w.Close()
+					wg.Done()
+				}()
+
+				_, err := stdcopy.StdCopy(w, w, readCloser)
+				if err != nil {
+					lh.Logger.Error("cannot split multiplex stream", "err", err)
+				}
+			}()
+
+			wg.Add(1)
+			go func() {
+				defer func() {
+					readCloser.Close()
+					wg.Done()
+				}()
+
+				scanner := bufio.NewScanner(r)
+				for scanner.Scan() {
+					line := scanner.Text()
+					if filter != nil {
+						if !filter.Match([]byte(line)) {
+							continue
+						}
+					}
+
+					lh.Logger.Debug("piping", "line", line)
+
+					_, err := lh.LogClients.Pipe.Write([]byte(line + "\n"))
+					if err != nil {
+						lh.Logger.Error("could not write to pipe", "err", err)
+					}
+				}
+
+				if err := scanner.Err(); err != nil {
+					lh.Logger.Error("error reading from container logs", "err", err)
+				}
+			}()
+
+			wg.Wait()
+
+			lh.Logger.Info("disconnected from logs, retrying", "container", lh.ContainerID)
+		}
+	}
+}
+
+func (lh *LogHandler) Close() {
+	lh.Cancel()
+}
+
+type LogClients struct {
+	Ctx          context.Context
+	Logger       *slog.Logger
+	DockerClient *client.Client
+	Pipe         *pipe.ReconnectReadWriteCloser
+	Clients      *syncmap.Map[string, *LogHandler]
+}
+
+func (lc *LogClients) AddContainer(ctx context.Context, logger *slog.Logger, containerID string) *LogHandler {
+	ctx, cancel := context.WithCancel(ctx)
+
+	handler, ok := lc.Clients.LoadOrStore(containerID, &LogHandler{
+		Ctx:         ctx,
+		Cancel:      cancel,
+		Logger:      logger,
+		ContainerID: containerID,
+		LogClients:  lc,
+	})
+
+	if !ok {
+		go handler.Handle()
+	}
+
+	return handler
+}
+
+func (lc *LogClients) RemoveContainer(containerID string) {
+	handler, ok := lc.Clients.Load(containerID)
+	if ok {
+		handler.Close()
+		lc.Clients.Delete(containerID)
+	}
+}
 
 func createDockerClient() *client.Client {
 	dockerClient, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
@@ -34,92 +194,6 @@ func createDockerClient() *client.Client {
 	return dockerClient
 }
 
-func containerStart(ctx context.Context, logger *slog.Logger, client *client.Client, reconn io.Writer, containerID string) error {
-	containerInfo, err := client.ContainerInspect(context.Background(), containerID)
-	if err != nil {
-		logger.Error(
-			"unable to inspect container info",
-			slog.Any("error", err),
-		)
-		return err
-	}
-
-	logger.Debug(
-		"container info",
-		slog.Any("container_info", containerInfo),
-	)
-
-	isEnabled := false
-	if enabledRaw, ok := containerInfo.Config.Labels["pipemgr.enable"]; ok {
-		if enabledRaw == "true" {
-			isEnabled = true
-		}
-	}
-
-	if !isEnabled {
-		return nil
-	}
-
-	filterStr := ""
-	if filterRaw, ok := containerInfo.Config.Labels["pipemgr.filter"]; ok {
-		filterStr = strings.TrimSpace(filterRaw)
-	}
-	filter, err := regexp.Compile(filterStr)
-	if err != nil {
-		logger.Error("invalid regex provided to pipemgr.filter", "err", err, "filter", filterStr)
-	}
-
-	logger.Info("connecting to logs", "container", containerID)
-	opts := ct.LogsOptions{
-		ShowStdout: true,
-		ShowStderr: true,
-		Follow:     true,
-		Since:      "0m",
-	}
-	readCloser, err := client.ContainerLogs(ctx, containerID, opts)
-	if err != nil {
-		logger.Error("unable to fetch container logs", "err", err)
-		return err
-	}
-
-	go func() {
-		r, w := io.Pipe()
-		defer r.Close()
-
-		go func() {
-			defer w.Close()
-
-			_, err = stdcopy.StdCopy(w, w, readCloser)
-			if err != nil {
-				logger.Error("cannot split multiplex stream", "err", err)
-			}
-		}()
-
-		scanner := bufio.NewScanner(r)
-		for scanner.Scan() {
-			line := scanner.Text()
-			if filter != nil {
-				if !filter.Match([]byte(line)) {
-					continue
-				}
-			}
-
-			logger.Debug("piping", "line", line)
-
-			_, err = reconn.Write([]byte(line + "\n"))
-			if err != nil {
-				logger.Error("could not write to pipe", "err", err)
-			}
-		}
-
-		if err := scanner.Err(); err != nil {
-			logger.Error("error reading from container logs", "err", err)
-		}
-	}()
-
-	return nil
-}
-
 func main() {
 	logLevelFlag := flag.String("log-level", "info", "Log level to set for the logger. Can be debug, warn, error, or info")
 	remoteHostFlag := flag.String("remote-host", "pipe.pico.sh:22", "The remote host to connect to in the format of host:port")
@@ -127,6 +201,8 @@ func main() {
 	remoteUserFlag := flag.String("remote-user", "", "The remote user to connect as")
 	keyLocationFlag := flag.String("remote-key-location", "/key", "The location on the filesystem of where to access the ssh key")
 	keyPassphraseFlag := flag.String("remote-key-passphrase", "", "The passphrase for an encrypted ssh key")
+	networkNames := flag.String("networks", "", "The networks to monitor for containers")
+	httpAddress := flag.String("http-address", "localhost:8080", "The address to bind the http server to")
 	command := flag.String("command", "pub container-drain -b=false", "The command to run for the remote session")
 
 	flag.Parse()
@@ -155,24 +231,31 @@ func main() {
 	dockerClient := createDockerClient()
 	defer dockerClient.Close()
 
-	hostname, err := os.Hostname()
-	if err != nil || hostname == "" {
-		rootLogger.Error(
-			"Unable to get hostname",
-			slog.Any("error", err),
-		)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	if *networkNames != "" {
+		networks = strings.Split(strings.ToLower(*networkNames), ",")
 	} else {
-		info, err := dockerClient.ContainerInspect(context.Background(), hostname)
-		if err != nil {
+		hostname, err := os.Hostname()
+		if err != nil || hostname == "" {
 			rootLogger.Error(
-				"unable to find networks. Please provide a list to monitor",
+				"Unable to get hostname",
 				slog.Any("error", err),
 			)
-			panic(err)
-		}
+		} else {
+			info, err := dockerClient.ContainerInspect(context.Background(), hostname)
+			if err != nil {
+				rootLogger.Error(
+					"unable to find networks. Please provide a list to monitor",
+					slog.Any("error", err),
+				)
+				panic(err)
+			}
 
-		for netw := range maps.Keys(info.NetworkSettings.Networks) {
-			networks = append(networks, strings.ToLower(strings.TrimSpace(netw)))
+			for netw := range maps.Keys(info.NetworkSettings.Networks) {
+				networks = append(networks, strings.ToLower(strings.TrimSpace(netw)))
+			}
 		}
 	}
 
@@ -183,7 +266,6 @@ func main() {
 		KeyLocation:    *keyLocationFlag,
 		KeyPassphrase:  *keyPassphraseFlag,
 	}
-	ctx := context.Background()
 
 	reconn := pipe.NewReconnectReadWriteCloser(
 		ctx,
@@ -213,84 +295,85 @@ func main() {
 		"key_passphrase", *keyPassphraseFlag,
 	)
 
+	logClients := &LogClients{
+		Ctx:          ctx,
+		Logger:       rootLogger,
+		DockerClient: dockerClient,
+		Pipe:         reconn,
+		Clients:      syncmap.New[string, *LogHandler](),
+	}
+
 	go func() {
 		http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 			w.WriteHeader(200)
 		})
 
-		err := http.ListenAndServe("localhost:8080", nil)
+		err := http.ListenAndServe(*httpAddress, nil)
 		if err != nil {
 			rootLogger.Error("error with http server", slog.Any("error", err))
+			cancel()
 		}
 	}()
 
-	go func() {
-		eventCtx, cancelEventCtx := context.WithCancel(context.Background())
-		clientEvents, errs := dockerClient.Events(eventCtx, events.ListOptions{})
+	clientEvents, errs := dockerClient.Events(ctx, events.ListOptions{})
 
-		containers, err := dockerClient.ContainerList(context.Background(), ct.ListOptions{})
-		if err != nil {
-			rootLogger.Error(
-				"unable to list container from docker",
-				slog.Any("error", err),
-			)
-		}
+	containers, err := dockerClient.ContainerList(ctx, ct.ListOptions{})
+	if err != nil {
+		rootLogger.Error(
+			"unable to list container from docker",
+			slog.Any("error", err),
+		)
+	}
 
-		for _, container := range containers {
-			logger := rootLogger.With("container", container.ID, "cmd", container.Command)
-			err = containerStart(ctx, logger, dockerClient, reconn, container.ID)
-			if err != nil {
-				logger.Error("cannot write to container", "err", err)
-			}
-		}
-
-		for {
-			select {
-			case event := <-clientEvents:
-				switch event.Type {
-				case events.ContainerEventType:
-					logger := slog.With(
-						slog.String("event", string(event.Action)),
-						slog.String("container_id", event.Actor.ID),
-					)
-					switch event.Action {
-					case events.ActionStart:
-						logger.Info("Received start")
-						err = containerStart(ctx, logger, dockerClient, reconn, event.Actor.ID)
-						if err != nil {
-							logger.Error("cannot write to container", "err", err)
-							break
-						}
-					case events.ActionDie:
-						logger.Info("Received die")
-					default:
-						logger.Debug(
-							"unhandled container action",
-							slog.Any("event_data", event),
-						)
-					}
-				default:
-					slog.Debug(
-						"unhandled daemon event",
-						slog.Any("event_data", event),
-					)
-				}
-			case err := <-errs:
-				cancelEventCtx()
-				slog.Error(
-					"error receiving events from daemon",
-					slog.Any("error", err),
-				)
-				panic(err)
-			}
-		}
-	}()
+	for _, container := range containers {
+		logger := rootLogger.With("container", container.ID, "cmd", container.Command)
+		logClients.AddContainer(ctx, logger, container.ID)
+	}
 
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
-	for s := range c {
-		slog.Info("signal recieved, exiting", slog.Any("signal", s))
-		break
+
+forLoop:
+	for {
+		select {
+		case event := <-clientEvents:
+			switch event.Type {
+			case events.ContainerEventType:
+				logger := slog.With(
+					slog.String("event", string(event.Action)),
+					slog.String("container_id", event.Actor.ID),
+				)
+				switch event.Action {
+				case events.ActionStart:
+					logger.Info("Received start")
+					logClients.AddContainer(ctx, logger, event.Actor.ID)
+				case events.ActionDie:
+					logger.Info("Received die")
+					logClients.RemoveContainer(event.Actor.ID)
+				default:
+					logger.Debug(
+						"unhandled container action",
+						slog.Any("event_data", event),
+					)
+				}
+			default:
+				slog.Debug(
+					"unhandled daemon event",
+					slog.Any("event_data", event),
+				)
+			}
+		case err := <-errs:
+			slog.Error(
+				"error receiving events from daemon",
+				slog.Any("error", err),
+			)
+			break forLoop
+		case s := <-c:
+			slog.Info("signal recieved, exiting", slog.Any("signal", s))
+			break forLoop
+		case <-ctx.Done():
+			slog.Info("context cancelled, exiting")
+			break forLoop
+		}
 	}
-	ctx.Done()
 }
